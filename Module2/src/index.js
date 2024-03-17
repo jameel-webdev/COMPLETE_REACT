7ee3cecde626e2a07887574733dba3997c3e5adb
Jameel Ahmed <jameel.webdev@gmail.com>
2024-03-17 12:10:02 +0530

    JSX & Components

--- Module2/src/index.js
+++ Module2/src/index.js
@@ -1,15 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsx = <div>JSX WORKING</div>;
-const div = React.createElement("div", { className: "react-text" }, [
-  "REACT",
-  jsx,
-]);
+const booLean = true;
+const trueText = "Im 20 years old";
+const falseText = "Im 15 years old";
+
+const handleClick = () => {
+  console.log("Iclicked");
+};
+
+const adult = (
+  <div>
+    {trueText} <span onClick={handleClick}>I have license</span>
+  </div>
+);
+const kid = (
+  <div>
+    {falseText} <span>I don have license</span>
+  </div>
+);
+
+const grownups = booLean ? adult : kid;
+
+const LicenseComponent = function () {
+  return (
+    <div>
+      {grownups}
+      <h3>This is the Functional Component</h3>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(div);
+root.render(<LicenseComponent />);
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
